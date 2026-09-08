2493106c88c4cf3c340c166dd850b90bfaeecf8d
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-09-08 23:52:13 +0300

    defaultMessage

---
+++ lau.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+test -d KindleUnpack || git clone https://github.com/kevinhendricks/KindleUnpack.git
+cd KindleUnpack/lib
+chmod 0755 kindleunpack.py
+echo -n "Introduceti calea catre fisierul awz3: "
+read bookpath
+echo -n "Introduceti calea catre directorul destinatie: "
+read outdir
+mkdir -p $outdir ${outdir}1
+test -f $bookpath || echo "Input file not found" && false
+./kindleunpack.py --epub_version=2 $bookpath ${outdir}1
+outfile=$(echo $bookpath|sed -e "s/azw3$/epub")
+cp -a ${outdir}1/mobi8/$outfile $outdir
+cd $outdir
+rm -rf ${outdir1}
+echo "Savat fisierul epub in $outdir/$outfile
