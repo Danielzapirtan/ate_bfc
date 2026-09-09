6b194b6b57c83e6a2159b19c372f3b22ae9e69da
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-09-09 07:59:35 +0300

    defaultMessage

--- app.py
+++ app.py
@@ -344,4 +344,4 @@ if __name__ == '__main__':
         print("Please ensure you have internet connection")
         exit(1)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5010)
